fa68cc9405fa57a504aef837076cfa5bcfebaecc
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-10-29 02:58:22 +0200

    add common networks

--- playground/app.vue
+++ playground/app.vue
@@ -1,26 +1,64 @@
 <template>
-  <div>
-    Nuxt module playground!
+  <div class="container">
+    <h1>Social Share Buttons</h1>
+
+    <h2>Styled</h2>
 
     <div class="buttons">
-      <SocialShare />
-      <SocialShare />
-      <SocialShare />
-      <SocialShare />
-      <SocialShare />
-      <SocialShare />
+      <SocialShare
+        v-for="network in testNetworks"
+        :key="network"
+        :network="network"
+        :styled="true"
+      />
+    </div>
+
+    <h2>Styled no labels</h2>
+
+    <div class="buttons">
+      <SocialShare
+        v-for="network in testNetworks"
+        :key="network"
+        :network="network"
+        :styled="true"
+        :label="false"
+      />
+    </div>
+
+    <h2>Unstyled (default)</h2>
+
+    <div class="buttons">
+      <SocialShare
+        v-for="network in testNetworks"
+        :key="network"
+        :network="network"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
+const testNetworks = [
+  'facebook',
+  'twitter',
+  'linkedin',
+  'pinterest',
+  'pocket',
+  'whatsapp',
+  'telegram',
+  'email',
+]
 </script>
 
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 .buttons {
   display: flex;
   gap: 0.5rem;
 }
-
 </style>
--- src/runtime/components/SocialShare.vue
+++ src/runtime/components/SocialShare.vue
@@ -1,13 +1,18 @@
 <template>
   <a
-    class="share-button styled"
-    :href="shareUrl"
+    class="share-button"
+    :class="[`network-${network}`, {'styled': styled}]"
+    :href="socialNetwork.shareUrl"
+    target="_blank"
   >
     <Icon
       class="share-icon"
-      icon="ri:facebook-fill"
+      :icon="socialNetwork.icon"
     />
-    <span class="share-label">Share</span>
+    <span
+      v-if="label" 
+      class="share-label"
+    >Share</span>
   </a>
 </template>
 
@@ -15,13 +20,64 @@
 import { Icon } from '@iconify/vue';
 import { useRequestURL } from '#imports';
 
-const pageUrl = useRequestURL().href
+const props = defineProps({
+  network: { type: String, required: true },
+  styled: { type: Boolean, default: false },
+  url: { type: String, default: null },
+  label: { type: Boolean, default: true },
+})
 
-const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
+const pageUrl = props.url || useRequestURL().href
 
+const networksMap = {
+  facebook : {
+    shareUrl: `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`,
+    icon: 'ri:facebook-fill'
+  },
+  twitter : {
+    shareUrl: `https://twitter.com/intent/tweet?url=${pageUrl}`,
+    icon: 'ri:twitter-fill'
+  },
+  linkedin : {
+    shareUrl: `https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`,
+    icon: 'ri:linkedin-fill'
+  },
+  pinterest : {
+    shareUrl: `http://pinterest.com/pin/create/button/?url=${pageUrl}`,
+    icon: 'jam:pinterest'
+  },
+  pocket : {
+    shareUrl: `https://getpocket.com/edit?url=${pageUrl}`,
+    icon: 'fe:pocket'
+  },
+  whatsapp : {
+    shareUrl: `https://api.whatsapp.com/send?text=${pageUrl}`,
+    icon: 'ri:whatsapp-line'
+  },
+  telegram : {
+    shareUrl: `https://t.me/share/url?url=${pageUrl}`,
+    icon: 'mingcute:telegram-fill'
+  },
+  email : {
+    shareUrl: `mailto:?body=${pageUrl}`,
+    icon: 'ic:round-mail'
+  },
+}
+
+const socialNetwork = networksMap[props.network]
 </script>
 
 <style lang="scss" scoped>
+$networks:
+  'facebook' #0866FF,
+  'twitter' #1D9BF0,
+  'linkedin' #0A66C2,
+  'pinterest' #BD081C,
+  'pocket' #EF3F56,
+  'whatsapp' #25D366,
+  'telegram' #26A5E4,
+  'email' #7e7e7e;
+
 .share-button {
   display: flex;
   gap: 0.5em;
@@ -34,11 +90,21 @@ const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
   }
 
   &.styled {
+    font-size: 0.875rem;
     padding: 0.5rem;
     color: white;
-    background-color: royalblue;
     border-radius: 0.25rem;
+    transition: all 0.25s ease-out;
 
+    @each $name, $color in $networks {
+      &.network-#{$name} {
+        background-color: $color;
+
+        &:hover {
+          background-color: darken($color, 20%);
+        }
+      }
+    }
     .share-label {
       padding: 0 0.5rem;
     }
