ab2b968b81ca267b78bf74ed9a2ecccd5c6a37c8
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-11-13 11:14:02 +0100

    feat: add `reddit` network

---
+++ src/runtime/networks/reddit.js
@@ -0,0 +1,7 @@
+export const reddit = {   
+  name: 'reddit',
+  shareUrl: 'https://www.reddit.com/submit?url=[u]',
+  iconName: 'ri:reddit-line',
+  color: '#FF4500'         
+}
+  
--- src/runtime/networksIndex.js
+++ src/runtime/networksIndex.js
@@ -2,18 +2,24 @@ import { facebook } from './networks/facebook';
 import { twitter } from './networks/twitter';
 import { linkedin } from './networks/linkedin';
 import { pinterest } from './networks/pinterest';
+import { reddit } from './networks/reddit';
 import { pocket } from './networks/pocket';
 import { whatsapp } from './networks/whatsapp';
 import { telegram } from './networks/telegram';
 import { email } from './networks/email';
 
 export const networksIndex = {
+  // Social Networks
   facebook,
   twitter,
   linkedin,
   pinterest,
+  reddit,
+  // Read it later
   pocket,
+  // Instant Messaging
   whatsapp,
   telegram,
+  // Other
   email
 }
