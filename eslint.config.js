bdd31e3f86d8ba840c090d19be0755be56517406
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-11-17 19:19:18 +0100

    revert: remove unwanted custom eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,4 @@ import nuxt from '@stefanobartoletti/eslint-config/nuxt.js'
 
 export default [
   ...nuxt,
-  {
-    rules: {
-      'vue/multi-word-component-names': 'warn',
-      'no-undef': 'error',
-      'vue/no-multiple-template-root': 'error'
-    },
-    }
-
 ]
