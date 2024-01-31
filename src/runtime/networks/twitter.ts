35d57cb6cc7bb7d29cd6cbcba6794635ae53a96f
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-01-31 12:41:36 +0100

    refactor: remove `@iconify/vue` dependency and use local icons definitions

--- src/runtime/networks/twitter.ts
+++ src/runtime/networks/twitter.ts
@@ -8,6 +8,10 @@ export const twitter: Network = {
     user: '&via=[uid]',
     hashtags: '&hashtags=[h]',
   },
-  iconName: 'ri:twitter-x-fill',
+  icon: {
+    // iconify: ri:twitter-x-fill
+    viewBox: '0 0 24 24',
+    path: 'M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z',
+  },
   color: '#000000',
 }
