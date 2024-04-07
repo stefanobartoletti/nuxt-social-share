27adb2e2435fc28a14091dc0a362989b19e3b3f3
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2024-04-08 07:37:36 +0900

    feat: support hatenabookmark

--- src/runtime/networksIndex.ts
+++ src/runtime/networksIndex.ts
@@ -10,6 +10,7 @@ import { whatsapp } from './networks/whatsapp'
 import { telegram } from './networks/telegram'
 import { skype } from './networks/skype'
 import { email } from './networks/email'
+import { hatenabookmark } from "./networks/hatenabookmark"
 
 export const networksIndex: NetworksIndex = {
   // Social Networks
@@ -18,6 +19,7 @@ export const networksIndex: NetworksIndex = {
   linkedin,
   pinterest,
   reddit,
+  hatenabookmark,
   // Read it later
   pocket,
   // Instant Messaging
