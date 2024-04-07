ef8ac70f9b7a8577a869913fc67ec47076ea7d49
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2024-04-08 07:35:05 +0900

    feat: support bluesky

--- src/runtime/networksIndex.ts
+++ src/runtime/networksIndex.ts
@@ -10,6 +10,7 @@ import { whatsapp } from './networks/whatsapp'
 import { telegram } from './networks/telegram'
 import { skype } from './networks/skype'
 import { email } from './networks/email'
+import { bluesky } from './networks/bluesky'
 
 export const networksIndex: NetworksIndex = {
   // Social Networks
@@ -18,6 +19,7 @@ export const networksIndex: NetworksIndex = {
   linkedin,
   pinterest,
   reddit,
+  bluesky,
   // Read it later
   pocket,
   // Instant Messaging
