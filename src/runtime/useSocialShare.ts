c5257886852268e0b1a14e6dd8998ec84e12d8f2
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-07-05 11:31:37 +0200

    fix: fix variable name typo

--- src/runtime/useSocialShare.ts
+++ src/runtime/useSocialShare.ts
@@ -16,17 +16,17 @@ export function useSocialShare(options: Options = defaultOptions) {
   const { network, url, title, user, hashtags, image } = options
 
   // Get network. Using a shallow copy to avoid mutating the original object
-  const selectedNework = ref({ ...networksIndex[network] })
+  const selectedNetwork = ref({ ...networksIndex[network] })
 
   // Set default value for url if not provided from options
   const pageUrl = url !== undefined ? url : useRequestURL().href
 
   // Build full share raw url
-  const shareUrl = selectedNework.value.shareUrl
-  const argTitle = (selectedNework.value.args?.title && title) ? selectedNework.value.args?.title : ''
-  const argUser = (selectedNework.value.args?.user && user) ? selectedNework.value.args?.user : ''
-  const argHashtags = (selectedNework.value.args?.hashtags && hashtags) ? selectedNework.value.args?.hashtags : ''
-  const argImage = (selectedNework.value.args?.image && image) ? selectedNework.value.args?.image : ''
+  const shareUrl = selectedNetwork.value.shareUrl
+  const argTitle = (selectedNetwork.value.args?.title && title) ? selectedNetwork.value.args?.title : ''
+  const argUser = (selectedNetwork.value.args?.user && user) ? selectedNetwork.value.args?.user : ''
+  const argHashtags = (selectedNetwork.value.args?.hashtags && hashtags) ? selectedNetwork.value.args?.hashtags : ''
+  const argImage = (selectedNetwork.value.args?.image && image) ? selectedNetwork.value.args?.image : ''
 
   let fullUrl = shareUrl + argTitle + argUser + argHashtags + argImage
 
@@ -38,9 +38,9 @@ export function useSocialShare(options: Options = defaultOptions) {
     .replace(/\[h\]/i, hashtags || '')
     .replace(/\[i\]/i, image || '')
 
-  // Rebuild selectedNework object
-  selectedNework.value.shareUrl = fullUrl
-  delete selectedNework.value.args
+  // Rebuild selectedNetwork object
+  selectedNetwork.value.shareUrl = fullUrl
+  delete selectedNetwork.value.args
 
-  return selectedNework
+  return selectedNetwork
 }
