fe886d548a21f7b166671874b6914a440d4a5296
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-07-05 11:13:11 +0200

    test: add compatibility date

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,12 +3,17 @@ export default defineNuxtConfig({
     '../src/module',
     '@nuxtjs/tailwindcss',
   ],
+
   socialShare: {
     // styled: true,
     // label: false,
   },
+
   devtools: { enabled: true },
+
   app: {
     baseURL: '/nuxt-social-share/',
   },
+
+  compatibilityDate: '2024-07-05',
 })
