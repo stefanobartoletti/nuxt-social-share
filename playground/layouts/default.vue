0a3906c9d25c52cdd41363da90e841af5a07e622
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-07-05 19:29:09 +0200

    test: refactor playground

---
+++ playground/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <SiteHeader />
+    <div class="flex-1">
+      <slot></slot>
+    </div>
+    <SiteFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SiteFooter from '../components/SiteFooter.vue'
+</script>
