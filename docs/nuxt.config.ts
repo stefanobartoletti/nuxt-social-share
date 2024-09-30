d18dca9856b94e4ffe8da418626bfeb52b09a65e
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-09-30 14:24:47 +0200

    docs: init docs

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,60 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+
+  uiPro: {
+    license: 'oss'
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxthq/studio',
+    'nuxt-og-image'
+  ],
+
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+
+  colorMode: {
+    disableTransition: true
+  },
+
+  routeRules: {
+    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+    '/': { prerender: true },
+    '/api/search.json': { prerender: true }
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  typescript: {
+    strict: false
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  compatibilityDate: '2024-07-11'
+})
